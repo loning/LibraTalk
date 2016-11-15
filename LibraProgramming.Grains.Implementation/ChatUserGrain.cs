a936028aa3fc7382cf7a37137dd39a4ccf349c01
Vladimir Tolmachev <tolmachewladimir@gmail.com>
2016-11-15 11:53:38 +0300

    NUget packages were updated

--- LibraProgramming.Grains.Implementation/ChatUserGrain.cs
+++ LibraProgramming.Grains.Implementation/ChatUserGrain.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading.Tasks;
 using LibraProgramming.Grains.Interfaces;
 using Orleans;
-using Orleans.Concurrency;
 using Orleans.Providers;
 using Orleans.Runtime;
 
